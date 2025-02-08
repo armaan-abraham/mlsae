c26c59c19053f3dba41b953931a26fe41faf772c
Armaan Abraham <armaan.abraham@hotmail.com>
2025-02-08 00:23:09 +0000

    experiment

--- src/mlsae/model/model_5.py
+++ src/mlsae/model/model_5.py
@@ -1,18 +1,18 @@
 from mlsae.model.model import DeepSAE
 
 
-class DeepSAE1(DeepSAE):
+class DeepSAE5(DeepSAE):
     def __init__(self, act_size: int, device: str = "cpu"):
         super().__init__(
             act_size=act_size,
             encoder_dim_mults=[1],
             sparse_dim_mult=16,
             decoder_dim_mults=[1],
-            name="1-1-1",
+            name="1-1-5",
             enc_dtype="fp32",
             device=device,
             topk=16,
-            act_decay_start=1e1,
+            act_decay_start=1e-4,
             act_decay_end=1e-9,
             act_decay_tau=1e9,
             weight_decay=2e-4,
--- src/mlsae/model/model_7.py
+++ src/mlsae/model/model_7.py
@@ -1,19 +1,19 @@
 from mlsae.model.model import DeepSAE
 
 
-class DeepSAE1(DeepSAE):
+class DeepSAE7(DeepSAE):
     def __init__(self, act_size: int, device: str = "cpu"):
         super().__init__(
             act_size=act_size,
             encoder_dim_mults=[1],
             sparse_dim_mult=16,
             decoder_dim_mults=[1],
-            name="1-1-1",
+            name="1-1-7",
             enc_dtype="fp32",
             device=device,
             topk=16,
-            act_decay_start=1e1,
-            act_decay_end=1e-9,
+            act_decay_start=0,
+            act_decay_end=0,
             act_decay_tau=1e9,
             weight_decay=2e-4,
             lr=2e-4,
