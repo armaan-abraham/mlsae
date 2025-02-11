39f09092a855050565a520b76718db7f3c1b748f
Armaan Abraham <armaan.abraham@hotmail.com>
2025-02-11 22:02:11 +0000

    update n blocks

--- src/mlsae/config.py
+++ src/mlsae/config.py
@@ -25,7 +25,7 @@ class DataConfig:
     dataset_column_name: str = "text"
     dataset_batch_size_entries: int = 50
 
-    n_token_blocks: int = 5
+    n_token_blocks: int = 6
     n_act_blocks: int = 2
 
     @property
