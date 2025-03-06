afb0511c6819a78a869a3c3a8867b1d8f5cf7503
Armaan Abraham <armaan.abraham@hotmail.com>
2025-03-06 17:59:35 +0000

    remove experiment

--- src/mlsae/model/experiment_models.py
+++ src/mlsae/model/experiment_models.py
@@ -57,21 +57,3 @@ class ExperimentSAERL2(RLSAE):
             prob_bias=-4,
             prob_deadness_penalty=5e-1,
         )
-
-class ExperimentSAERL3(RLSAE):
-    def __init__(self, act_size: int, device: str = "cpu"):
-        super().__init__(
-            act_size=act_size,
-            device=device,
-            encoder_dim_mults=[],
-            sparse_dim_mult=8,
-            decoder_dim_mults=[],
-            lr=1e-3,
-            temperature_initial=1,
-            temperature_final=1,
-            num_samples=10,
-            L0_penalty=1e-5,
-            rl_loss_weight=8e-3,
-            prob_bias=-4,
-            prob_deadness_penalty=2.5e-1,
-        )
