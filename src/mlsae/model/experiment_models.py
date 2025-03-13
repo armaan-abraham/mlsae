01a5cdea9c440469eea6b6ddf6878c75b1a1ffb3
Armaan Abraham <armaan.abraham@hotmail.com>
2025-03-13 20:28:49 +0000

    wd

--- src/mlsae/model/experiment_models.py
+++ src/mlsae/model/experiment_models.py
@@ -9,9 +9,9 @@ class ExperimentSAERL1_0(RLSAE):
     def __init__(self, act_size: int, device: str = "cpu"):
         super().__init__(
             act_size=act_size,
-            encoder_dim_mults=[1],
+            encoder_dim_mults=[1, 1],
             sparse_dim_mult=16,
-            decoder_dim_mults=[1],
+            decoder_dim_mults=[1, 1],
             device=device,
             num_samples=10,
             L0_penalty=1e-5,
@@ -24,6 +24,7 @@ class ExperimentSAERL1_0(RLSAE):
             },
             optimize_steps=3,
             ppo_clip=0,
+            weight_decay=1e-2,
         )
 
 
