e36c3b57cffb68f677e98db4473f6dcb87ed0341
Armaan Abraham <armaan.abraham@hotmail.com>
2025-03-24 22:34:28 +0000

    add factory

--- src/mlsae/model/experiment_models.py
+++ src/mlsae/model/experiment_models.py
@@ -14,7 +14,7 @@ class ExperimentSAERL(RLSAE):
             decoder_dim_mults=[],
             device=device,
             num_samples=5,
-            L0_penalty=2e-5,
+            L0_penalty=5e-5,
             rl_loss_weight=0.2,
             optimizer_type="sparse_adam",
             optimizer_config={
@@ -23,25 +23,65 @@ class ExperimentSAERL(RLSAE):
             optimize_steps=1,
 
             base_L0=2,
-            action_collapse_penalty_lambda=2e-4,
+            action_collapse_penalty_lambda=0,
         )
 
-# class ExperimentSAETopk(ExperimentSAEBase):
-#     def __init__(self, act_size: int, device: str = "cpu"):
-#         super().__init__(
-#             act_size=act_size,
-#             device=device,
-#             encoder_dim_mults=[],
-#             sparse_dim_mult=8,
-#             decoder_dim_mults=[],
-#             topk_init=2,
-#             topk_final=2,
-#             topk_decay_iter=1000,
-#             act_squeeze=0,
-#             weight_decay=0,
-#             optimize_steps=1,
-#             optimizer_type="sparse_adam",
-#             optimizer_config={
-#                 "lr": 2e-3,
-#             }
-#         )
+def create_model_variants(base_class, param_grid, prefix="ExperimentSAE"):
+    """
+    Dynamically creates model classes with varying hyperparameters.
+    
+    Args:
+        base_class: The base class to inherit from
+        param_grid: Dictionary mapping param names to lists of values
+        prefix: Prefix for the generated class names
+    
+    Returns:
+        List of created model classes
+    """
+    created_classes = []
+    
+    # Generate all combinations of hyperparameters
+    import itertools
+    param_names = list(param_grid.keys())
+    param_values = list(param_grid.values())
+    
+    for i, values in enumerate(itertools.product(*param_values)):
+        params = dict(zip(param_names, values))
+        
+        # Create a descriptive name based on the params
+        param_str = "_".join(f"{k}_{v}".replace(".", "p") for k, v in params.items())
+        class_name = f"{prefix}{base_class.__name__}_{param_str}"
+        
+        # Create a new class with these parameters
+        def create_init(fixed_params):
+            def __init__(self, act_size: int, device: str = "cpu", **kwargs):
+                # Merge the fixed params with any additional kwargs
+                all_params = {**fixed_params, **kwargs}
+                super(self.__class__, self).__init__(
+                    act_size=act_size,
+                    device=device,
+                    **all_params
+                )
+            return __init__
+        
+        # Create the new class
+        new_class = type(class_name, (base_class,), {
+            "__init__": create_init(params),
+            "__doc__": f"Auto-generated variant of {base_class.__name__} with {params}"
+        })
+        
+        # Add the class to the module's global namespace
+        globals()[class_name] = new_class
+        created_classes.append(new_class)
+    
+    return created_classes
+
+# You can also create variants of your ExperimentSAERL class
+# Uncomment if needed:
+# rl_experiment_variants = create_model_variants(
+#     ExperimentSAERL,
+#     {
+#         "num_samples": [3, 5, 10],
+#         "rl_loss_weight": [0.1, 0.2, 0.3]
+#     }
+# )
